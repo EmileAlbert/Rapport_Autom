766fe8cd66b60dc9193ab4e625e477c83294ab64
Mathieu David <mathieudavid@mathieudavid.org>
2018-05-04 15:14:43 +0200

    Compléter séance 2

--- Matlab Data/Laboratoire 2/bodeOfG0.m
+++ Matlab Data/Laboratoire 2/bodeOfG0.m
@@ -1,20 +1,38 @@
 function a = bodeOfG0(Kp,Ti,Td,v, name)
-figure
-s = tf('s');
-Hvdg = (0.98*exp(-s*14.97))/((s*14.74+1)*(s*7.12+1));
-Tf = Td/v;
-R = Kp*(((1+s*Ti)/s*Ti)*((s*Td+1)/(s*Tf+1)));
-G0 = Hvdg*R;
-[Am,OmegaA,phi,Omegaphi] = margin(G0);
-%logspace(a,b) -> logarithmic space between 10^a and 10^b
-w = logspace(-2, 3);
-bode(G0,w);
-formatAm = 'Am = %f';
-formatOmegaA = 'Omega Am = %f rad/s';
-formatphi = 'Phi = %f';
-formatOmegaphi = 'OmegaPhi = %f rad/s';
-title({sprintf(formatAm,Am),sprintf(formatOmegaA,OmegaA),sprintf(formatphi,phi),sprintf(formatOmegaphi,Omegaphi)});
-grid on
-print(name, '-dpng');
+    figure('rend','painters','pos',[10 10 1200 700])
+    s = tf('s');
+    
+    Kv = 0.98;
+    Tm = 14.97;
+    T1 = 14.74;
+    T2 = 7.12;
+    
+    Hvdg = (Kv * exp(-s * Tm)) / ((s * T1 + 1)*(s * T2 + 1));
+    
+    Tf = 1; %Td / v;
+    
+    R = Kp * (((1+s*Ti)/s*Ti) * ((s*Td+1)/(s*Tf+1)));
+    
+    G0 = Hvdg*R;
+    
+    [Gm,Pm,Wgm,Wpm] = margin(G0);
+    %margin(G0);
+    %pause;
+    %logspace(a,b) -> logarithmic space between 10^a and 10^b
+    w = logspace(-2, 2);
+    
+    %[~, phase, wout] = bode(G0);
+    %limit360 = find(phase <= -360);
+    %w360 = log10(wout(limit360(1)))
+    %w360 = find(wout >= max(Wgm, Wpm), 1)
+    
+    %w = logspace(-2, w360);
+    
+    bode(G0, w);
+    formatGm = 'Gm = %f dB (à %f rad/s)';
+    formatPm = 'Pm = %f deg (à %f rad/s)';
+    title({sprintf(formatGm, Gm, Wgm), sprintf(formatPm, Pm, Wpm)});
+    grid on
+    print(name, '-dpng');
 end
 
